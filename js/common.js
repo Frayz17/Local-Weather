2d4e57abe8905aef88d98000a8253985d5c1e22c
Andrey Dmitriew <andrydmit@gmail.com>
2017-09-08 23:55:11 +0300

    parse JSON string into weather object

--- js/common.js
+++ js/common.js
@@ -1,27 +1,54 @@
-var temperature = getElementById('#temperature');
-var location = getLocation();
+var temperature = document.getElementById('temperature');
+var latitude;
+var longitude;
+var addressWeatherApi;
+var weather;
 
-var latitude = location.coords.latitude;
-var longitude = location.coords.longitude;
-
-temperatur.innerHTML = "latitude: " + latitude + ", longitude: " + longitude;
-
-// var weatherData = getJSON();
-
-
-
-
-
-
-
-function getLocation() {
-	var location;
-
-	if (navigator.geolocation) {
-		location = navigator.geolocation.getCurrentPosition;
-	} else {
-		location = "<p>Geolocation is not supported by this browser.<p>"
-	}
-
-	return location;
+if (!navigator.geolocation) {
+	temperature.innerHTML = "<p>Geolocation is not supported by this browser.<p>";
 }
+
+
+navigator.geolocation.getCurrentPosition(success, error);
+
+
+function success(position) {
+	latitude = position.coords.latitude;
+	longitude = position.coords.longitude;
+	addressWeatherApi = "https://fcc-weather-api.glitch.me/api/current?lon=" +
+												 + latitude + "&lat=" + longitude;
+
+
+	var request = new XMLHttpRequest();
+	request.open('GET', addressWeatherApi, true);
+
+	request.onload = function() {
+	  if (request.status >= 200 && request.status < 400) {
+	    // Success!
+	    weather = JSON.parse(request.responseText);
+	  } else {
+	    // We reached our target server, but it returned an error
+	    console.log("We reached our target server, but it returned an error");
+	  }
+	};
+
+	request.onerror = function() {
+	  // There was a connection error of some sort
+	  console.log("There was a connection error of some sort");
+	};
+
+	request.send();
+
+
+	temperature.innerHTML = "latitude: " + latitude + ", longitude: " + longitude;
+
+}
+
+
+function error() {
+    temperature.innerHTML = "Unable to retrieve your location";
+}
+
+
+
+
